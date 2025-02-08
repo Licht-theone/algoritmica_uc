7bc3e99ad177b61b6ce1dd5db021b3da5af620ee
itzznightyy <oscarcuevaslopez@gmail.com>
2025-02-08 15:18:43 +0100

    Añadida la practica 1

---
+++ src/pract01/pract01.py
@@ -0,0 +1,13 @@
+
+# TODO: Implementar el algoritmo
+def sumaDist(arr) :
+    return 0
+
+# Main del programa
+def main():
+    # TODO: implementar el main adecuadamente
+    print("Hola Mundo!")
+
+# Ejecuta la funcion main en el caso de que se ejecute como un programa principal
+if __name__ == "__main__":
+    main()

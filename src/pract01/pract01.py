6fce3773abc5ced87571039f1b22b57c687e1908
Oscar Cuevas <oscarcuevaslopez@gmail.com>
2025-02-11 20:01:19 +0100

    Añadida la funcionalidad de medir el tiempo con un ejemplo

--- src/pract01/pract01.py
+++ src/pract01/pract01.py
@@ -24,11 +24,15 @@ def suma_dist(arr):
 
 # Main del programa
 def main():
-    # TODO: implementar el main adecuadamente
-    arr1 = [1] * 1000
-    arr2 = [5,4,3,2,1]
+    # Listas para probar el algoritmo
+    arr1 = [1] * 10000
+    arr2 = [6,2,3,4,5]
+    
+    ## EJEMPLO DE USAR TIME ##
+    t = time.time()
     print(suma_dist(arr2))
-
+    T = time.time() - t
+    print("Tiempo de ejecucion: " + str(T) + "s")
 
 # Ejecuta la funcion main en el caso de que se ejecute como un programa principal
 if __name__ == "__main__":

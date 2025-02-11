b30dbedbc2016ab32b1475e488cd42b0830aeb40
itzznightyy <oscarcuevaslopez@gmail.com>
2025-02-11 18:56:21 +0100

    Añadida la funcion y una pequeña bateria de tests

--- src/pract01/pract01.py
+++ src/pract01/pract01.py
@@ -1,12 +1,34 @@
+import time
 
-# TODO: Implementar el algoritmo
-def sumaDist(arr) :
-    return 0
+def suma_dist(arr):
+    n = len(arr)
+    suma_distancia = 0
+    
+    for i in range(n):
+        num = arr[i]
+        dist = 0
+        
+        # busca el primer numero a la derecha que sea mayor
+        for j in range(i + 1, n):
+            dist += 1
+            if arr[j] > num:
+                break
+        else:
+            # si no se encuentra un numero mayor, se determina que existe otro mayor al final de la lista
+            dist += 1
+
+        #suma la distancia
+        suma_distancia += dist 
+    
+    return suma_distancia
 
 # Main del programa
 def main():
     # TODO: implementar el main adecuadamente
-    print("Hola Mundo!")
+    arr1 = [1] * 1000
+    arr2 = [5,4,3,2,1]
+    print(suma_dist(arr2))
+
 
 # Ejecuta la funcion main en el caso de que se ejecute como un programa principal
 if __name__ == "__main__":
